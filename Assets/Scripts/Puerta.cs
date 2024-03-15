c64f359c28c7144dadc1eb9e2c8c6e3397d32961
pokoli0 <psierr01@ucm.es>
2024-03-15 19:48:55 +0100

    interacciones uou

---
+++ Assets/Scripts/Puerta.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : InteractorBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void Interact()
+    {
+        //ACCION DE LA PUERTA
+        Debug.Log("interaactoo");
+    }
+}
