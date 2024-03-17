01e250a4242ae085b0b4ed2248ae143a73886a87
Jesus Gonzalez <jesugo11@ucm.es>
2024-03-17 17:55:58 +0100

    MAs cositas jij

---
+++ Assets/cambioDeEscena.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class cambioDeEscena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string nombreDeLaEscenaACargar = "IDK Scene"; // El nombre de la escena que deseas cargar
+
+    // Método para cargar la nueva escena
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Invoke("CargarEscena",3.0f);
+    }
+public void CargarEscena()
+    {
+        SceneManager.LoadScene(nombreDeLaEscenaACargar);
+    }
+}
+
+    
